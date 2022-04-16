61175c0551c074aa47aaade796322158ef4f69b0
Adil Ansari <68107671+adilansari488@users.noreply.github.com>
2022-04-17 00:00:42 +0530

    Update hash13Download.py

--- hash13Download.py
+++ hash13Download.py
@@ -1,7 +1,7 @@
 from tqdm import tqdm
 import requests
 
-v2 = 'aws part3' # text filename of links
+v2 = 'v2' # text filename of links
 
 file = open(v2,'r')
 lines = file.readlines()
@@ -16,40 +16,3 @@ with open(filename, 'ab') as file2 :
         # open(filename, 'ab').write(r.content)
 print("Download Completed.")
 file.close()
-
-
-# --------------------------------------------------------------------------------
-
-
-
-# downloads = int((len(lines) - 7) / 2)
-# downloaded = 0
-# for i in range(7,length,2) :
-#     progress = (downloaded/downloads)*100
-#     downloaded+=1
-#     print(progress,'%')
-
-
-
-
-
-
-
-# #
-# for line in lines :
-#     print(line.strip())
-# file.close()
-#
-# # url = 'https://embed-fastly.wistia.com/deliveries/5f543d377eee6f9a90d449cf45fa033942ab5a18.m3u8/v2/seg-1264-v1-a1.ts'
-# # r = requests.get(url, allow_redirects=True)
-# # open('try1.ts', 'wb').write(r.content)
-# #
-# #
-# #
-# # file2 = open('seg-1-v1-a1.ts','rb')
-# #
-# # with open('try1.ts','ab') as file :
-# #     file.write(file2.read())
-# # file2.close()
-# #
-# #
